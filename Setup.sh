fcb15be433d00ae85f145760e0d40c12a90cc3e4
Dark Zone <62567666+25225428-Mr-Virus@users.noreply.github.com>
2020-07-26 16:14:49 +0000

    Add files via upload

---
+++ Setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#This is simple Cleaner that made by Mr.Virus
+
+#Colors
+
+RED='\033[0;31m'
+ORANGE='\033[0;33-'
+GREEN='\033[0;32m'
+NC='\033[0m' #No color
+
+#Make sure you run this script as root user
+if [ "$EUID" -ne 0 ];
+then
+	clear
+	date
+	figlet -c "Installing "
+        echo -e
+        echo -e $RED"Please must be as root"$NC
+        exit
+fi
+
+#installing and setting up directories.
+
+clear
+date
+figlet -c "Installing "
+
+echo -e
+echo -e "This is simple P-Cleaner that made by Mr.Virus "
+echo -e
+sleep 1s
+echo -e $GREEN"Installing P-Cleaner "
+echo -e $GREEN"Setting Up Directories "$NC
+echo -e
+cd $HOME
+
+sudo apt clean
+sudo apt autoclean
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install aptitude
+sudo apt-get install -y git
+
+git clone https://github.com/25225428-Mr-Virus/P-Cleaner
+sleep 1s
+cd P-Cleaner
+sudo chmod 744 P-Cleaner.sh
+sudo cp P-Cleaner /usr/bin/
+sudo chmod 744 /usr/bin/P-Cleaner
+sleep 2s
+echo -e $ORANGE"To run this type P-Cleaner in your Terminal. "$NC
+
+echo -e "Exiting.... "
+echo -e $RED"Goodbyee.... "$NC
